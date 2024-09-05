05bdbdce04d4498c8b02cf4a001deb2a64a86356
Adnan A S <77780496+asadnan@users.noreply.github.com>
2024-09-05 19:29:37 +0530

    Update dashboard/page.tsx

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 
-const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
+import { cookies } from 'next/headers';
+import db from '@/lib/supabase/db';
+import { redirect } from 'next/navigation';
+
+const Dashboard = async () => {
+  const supabase = createServerComponentClient({ cookies });
+
+  const { data: { user }} = await supabase.auth.getUser();
+  if (!user) return;
+
+  const workspace = await db.query.workspaces.findFirst({
+    where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id),
+  });
+  if(!workspace) return(
+    <div className="bg-background h-screen w-screen flex justify-center items-center">
+      {/* <DashboardSetup /> */}
+    </div>
   );
+
+
+  redirect(`/dashboard/${workspace.id}`);
 };
 
 export default Dashboard;
