27b368ba7a00334693fe9f362590adcf1a529d25
Adnan A S <77780496+asadnan@users.noreply.github.com>
2024-09-06 00:24:57 +0530

    Fix typo

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -7,5 +7,5 @@ export const FormSchema = z.object({
 
 export const CreateWorkspaceFormSchema = z.object({
     workspaceName: z.string().describe('Workspace Name').min(1,"Workspace name must be min of 1 character"),
-    file: z.any(),
+    logo: z.any(),
 });
