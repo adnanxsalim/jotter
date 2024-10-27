fc4d4bfa1cb0450b2372d239b52e62197db18d59
Adnan A S <77780496+asadnan@users.noreply.github.com>
2024-10-27 17:32:44 +0530

    Update WorkspaceDropdown.tsx component

--- src/components/sidebar/WorkspaceDropdown.tsx
+++ src/components/sidebar/WorkspaceDropdown.tsx
@@ -30,6 +30,11 @@ const WorkspaceDropdown: React.FC<WorkspaceDropdownProps> = ({ privateWorkspaces
         setIsOpen(false);
     }
 
+    useEffect(() => {
+        const findSelectedWorkspace = state.workspaces.find(workspace => workspace.id === defaultValue.id)
+        if(findSelectedWorkspace) setSelectedOption(findSelectedWorkspace)
+    }, [state, defaultValue])
+
     return (
       <div className="relative inline-block text-left">
         <div>
