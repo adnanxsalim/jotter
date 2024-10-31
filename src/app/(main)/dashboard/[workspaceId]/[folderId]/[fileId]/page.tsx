b3472083be4c9605d6df7c25a014c3cfd7450e31
Adnan A S <77780496+asadnan@users.noreply.github.com>
2024-10-31 22:43:57 +0530

    Update file page.tsx

--- src/app/(main)/dashboard/[workspaceId]/[folderId]/[fileId]/page.tsx
+++ src/app/(main)/dashboard/[workspaceId]/[folderId]/[fileId]/page.tsx
@@ -1,8 +1,18 @@
+export const dynamic = 'force-dynamic'
+
+import QuillEditor from '@/components/quill-editor/QuillEditor'
+import { getFileDetails } from '@/lib/supabase/queries'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const FilePage = () => {
+const FilePage = async ({ params }: { params: { fileId: string } }) => {
+  const { data, error } = await getFileDetails(params.fileId)
+  if(error || !data.length) redirect('/dashboard')
+
   return (
-    <div>FilePage</div>
+    <div className="relative">
+      <QuillEditor dirType="file" fileId={params.fileId} dirDetails={data[0] || {}} />
+    </div>
   )
 }
 
